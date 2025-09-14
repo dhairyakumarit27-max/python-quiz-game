1d5b5d702dc60a84c4b0cd0d27b71f17666600b5
Dhairya Kumar <dhairyakumar.it27@jecrc.ac.in>
2025-09-14 20:01:04 +0530

    Initial commit - Quiz project

--- practise3.py
+++
@@ -1,9 +0,0 @@
-#function defination
-def double_number(x):
- result=x*2
- return result 
-
-
-#using the function
-a = int(input("enter the number:"))
-print("the double of this number is:", double_number(a))
